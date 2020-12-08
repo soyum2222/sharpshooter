aca4f4541aa312f54db3c97b2353ee7e1ef2b229
soyum2222 <shu123green@qq.com>
2020-12-08 18:41:18 +0800

    transfer

--- example/sharp_transfer.go
+++ example/sharp_transfer.go
@@ -3,12 +3,14 @@ package main
 import (
 	"encoding/binary"
 	"flag"
+	"fmt"
 	"github.com/soyum2222/sharpshooter"
 	"io"
 	"log"
 	"net"
 	"os"
 	"strconv"
+	"time"
 )
 
 var l int
@@ -18,6 +20,7 @@ var addr string
 var i string
 
 var c bool
+var t bool
 
 func main() {
 	flag.IntVar(&l, "l", 0, "listen port")
@@ -25,21 +28,42 @@ func main() {
 	flag.StringVar(&o, "o", "", "output file path")
 	flag.StringVar(&addr, "addr", "", "remote ip addr IP:PORT")
 	flag.StringVar(&i, "i", "", "input file path")
+	flag.BoolVar(&t, "t", false, "use tcp")
 	flag.Parse()
+
+	log.SetFlags(log.Lshortfile)
 
 	var conn net.Conn
 	var err error
 	if addr != "" {
-		conn, err = sharpshooter.Dial(addr)
-		if err != nil {
-			log.Println(err)
-			os.Exit(1)
+		if t {
+			conn, err = net.Dial("tcp", addr)
+			if err != nil {
+				log.Println(err)
+				os.Exit(1)
+			}
+		} else {
+			conn, err = sharpshooter.Dial(addr)
+			if err != nil {
+				log.Println(err)
+				os.Exit(1)
+			}
 		}
+
 	} else {
-		listen, err := sharpshooter.Listen(":" + strconv.Itoa(l))
-		if err != nil {
-			log.Println(err)
-			os.Exit(1)
+		var listen net.Listener
+		if t {
+			listen, err = net.Listen("tcp", ":"+strconv.Itoa(l))
+			if err != nil {
+				log.Println(err)
+				os.Exit(1)
+			}
+		} else {
+			listen, err = sharpshooter.Listen(":" + strconv.Itoa(l))
+			if err != nil {
+				log.Println(err)
+				os.Exit(1)
+			}
 		}
 
 		conn, err = listen.Accept()
@@ -87,21 +111,52 @@ func send(conn net.Conn, i string) {
 		os.Exit(1)
 	}
 
+	var totalSize int64
+	totalSize = info.Size()
+
 	_, err = file.Seek(off, 0)
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
 
-	_, err = io.Copy(conn, file)
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
+	b := make([]byte, 1024*32)
+	var size int64
+
+	go func() {
+		var old int64
+		for {
+			time.Sleep(time.Millisecond * 500)
+			printBar(totalSize, size)
+			fmt.Printf("%d KB/s \n", ((size-old)/1024)*2)
+			old = size
+		}
+	}()
+
+	for {
+		n, err := file.Read(b)
+		if err != nil && err != io.EOF {
+			log.Println(err)
+			break
+		}
+
+		if n == 0 {
+			break
+		}
+
+		_, err = conn.Write(b[:n])
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		size += int64(n)
 	}
+
+	printBar(totalSize, size)
 
 	_ = conn.Close()
 
-	os.Exit(1)
+	os.Exit(0)
 }
 
 func receive(conn net.Conn, o string, c bool) {
@@ -156,11 +211,75 @@ func receive(conn net.Conn, o string, c bool) {
 		os.Exit(1)
 	}
 
-	size := binary.BigEndian.Uint64(remain)
+	var total int64
+	total = int64(binary.BigEndian.Uint64(remain)) + offset
 
-	n, err := io.Copy(file, conn)
-	if err != nil && n != int64(size) {
-		log.Println(err)
-		os.Exit(1)
+	b := make([]byte, 1024*32)
+	var size int64
+
+	go func() {
+		var old int64
+		for {
+			time.Sleep(time.Millisecond * 500)
+			printBar(total, size)
+			fmt.Printf("%d KB/s \n", ((size-old)/1024)*2)
+			old = size
+		}
+	}()
+
+	for {
+		n, err := conn.Read(b)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		if n == 0 {
+			break
+		}
+
+		_, err = file.Write(b[:n])
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		size += int64(n)
+
 	}
+
+	printBar(total, size)
+	conn.Close()
+	os.Exit(0)
+
+}
+
+func progressBar(total, size int64) [100]bool {
+
+	p := float64(size) / float64(total) * 100
+
+	pro := [100]bool{}
+	for i := 0; i < int(p); i++ {
+		pro[i] = true
+	}
+	return pro
+}
+
+func printBar(total, size int64, ) {
+
+	bar := progressBar(total, size)
+
+	fmt.Print("[")
+	n := 0
+	for i := 0; i < 100; i++ {
+		if bar[i] {
+			fmt.Print("*")
+			n++
+		} else {
+			fmt.Printf("=")
+		}
+	}
+
+	fmt.Printf("]	%d %% \n", n)
+	fmt.Printf("		total: %d byte		transfer: %d byte \n", total, size)
 }
